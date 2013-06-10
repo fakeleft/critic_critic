8105b04fe41d8ce8b4d8d7aeeb7cfab8761a3ece
Dustin Zeisler <dustin@zive.me>
2013-06-10 15:48:48 -0700

    New rails project with haml and bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CriticCritic::Application.config.secret_key_base = 'fad40229a6d2bc329da0886a88264a5b6b4680b769ff5dec2098eefc27b479f0eb802d1093b5579a362fdc719c65ebd1de2d69f08fec9848c1e24a55673aa14c'
