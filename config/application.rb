53150687cc79e628826c88674206798afbf5c202
Dustin Zeisler <dustin@zive.me>
2013-06-10 17:01:36 -0700

    initial design mock: static_pages movie.html

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,11 @@ module CriticCritic
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # Bootstrap for Sass
+    # Due to a change in Rails that prevents images from being compiled
+    # in vendor and lib, you'll need to add the following line to your application.rb:
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
 
-# Bootstrap for Sass
-# Due to a change in Rails that prevents images from being compiled
-# in vendor and lib, you'll need to add the following line to your application.rb:
-config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CriticCritic::Application.routes.draw do
+  get "static_pages/movies"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
