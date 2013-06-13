876e47405f575f18ed808eb393acfde15db48a4d
Jordan Skarbek <jaskarbek@gmail.com>
2013-06-13 15:43:32 -0700

    corrected has_many though in movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,6 @@ class Movie < ActiveRecord::Base
   validates :year, presence: true, numericality: true
 
   has_many :critic_opinions
-  has_many :critics, :through => :critic_reviews
+  has_many :critics, :through => :critic_opinions
   has_many :users, :through => :user_opinions
 end
