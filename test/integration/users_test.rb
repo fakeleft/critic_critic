b17bd1ffd80962587324b6c455935a9197e85a99
Jordan Skarbek <jaskarbek@gmail.com>
2013-06-17 00:22:58 -0700

    converted user view files to haml

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -6,6 +6,8 @@ class UsersViewTest < ActionDispatch::IntegrationTest
     describe 'Users home' do
 
       it "should have the title 'Movie Critic | All Users'"
+        visit '/users'
+        page.must_have_title('Movie Critic | All Users')
 
       it "should have h1 'All Users'"
 
