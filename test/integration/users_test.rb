5293a7a8c1c8ef2d0ae333d2f38cde6d33a2a72c
Jordan Skarbek <jaskarbek@gmail.com>
2013-06-16 23:46:06 -0700

    improved text in h1 elements across user views. modified relevant integration tests.

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -5,9 +5,9 @@ class UsersViewTest < ActionDispatch::IntegrationTest
   describe 'Users pages' do
     describe 'Users home' do
 
-      it "should have the title 'Movie Critic | Listing Users'"
+      it "should have the title 'Movie Critic | All Users'"
 
-      it "should have h1 'Listing users'"
+      it "should have h1 'All Users'"
 
     end
   end
