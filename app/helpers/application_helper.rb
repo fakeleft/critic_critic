635dec8396ef23563bc1898dde9810b071edaefa
Jordan Skarbek <jaskarbek@gmail.com>
2013-06-17 15:52:23 -0700

    created application helper to handle thumbs up/down logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def thumbs(boolean)
+      if boolean
+        class_select = "icon-thumbs-up"
+      else
+        class_select = "icon-thumbs-down"
+      end
+      return { class: "#{class_select}" }
+  end
 end
