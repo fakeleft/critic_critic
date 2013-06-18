9423228ebd7f5f8ef21427cbc78e05de1bae3fbd
theoretick <mail@limitlesschannels.com>
2013-06-18 15:52:06 -0700

    adds VCR for mock api calls. Moves gemfile entries for testing under test/dev group

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
 require 'turn'
+require 'vcr_setup'
+
 
 class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
--- test/unit/api_seed_generator_test.rb
+++ test/unit/api_seed_generator_test.rb
@@ -9,23 +9,35 @@ describe 'ApiSeedGenerator' do
   it 'should get an api key' do
     @seed_generator.api_key.class.must_equal String
   end
+
   it 'should get the details for one movie by title' do
-    movie = @seed_generator.search_movies("Fried&Green&Tomatoes", 1)
-    movie.class.must_equal Hash
-    movie["movies"][0]["title"].must_equal "Fried Green Tomatoes"
+    VCR.use_cassette('seed_search_movies') do
+      movie = @seed_generator.search_movies("Fried&Green&Tomatoes", 1)
+      movie.class.must_equal Hash
+      movie["movies"][0]["title"].must_equal "Fried Green Tomatoes"
+    end
   end
+
   it 'should get the details for one movie by id' do
-    movie = @seed_generator.get_movie(10104)
-    movie["title"].must_equal "Fried Green Tomatoes"
+    VCR.use_cassette('seed_get_details_for_one_movie_by_id') do
+      movie = @seed_generator.get_movie(10104)
+      movie["title"].must_equal "Fried Green Tomatoes"
+    end
   end
+
   it 'should get the reviews for one movie by id' do
-    reviews = @seed_generator.get_reviews(10104)
-    reviews["reviews"].wont_be_nil
+    VCR.use_cassette('seed_get_reviews_for_one_movie_by_id') do
+      reviews = @seed_generator.get_reviews(10104)
+      reviews["reviews"].wont_be_nil
+    end
   end
+
   it 'should get a list of upcoming movie ids' do
-    movie_ids = @seed_generator.get_movie_ids
-    movie_ids.class.must_equal Array
-    movie_ids.empty?.must_equal false
-    movie_ids.length.must_equal 10
+    VCR.use_cassette('seed_get_upcoming_movie_ids') do
+      movie_ids = @seed_generator.get_movie_ids
+      movie_ids.class.must_equal Array
+      movie_ids.empty?.must_equal false
+      movie_ids.length.must_equal 10
+    end
   end
 end
