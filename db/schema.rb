05bfc8be9e26cc8b097362e149d437990a4eaa93
Dustin Zeisler <dustin@zive.me>
2013-06-19 12:46:36 -0700

    db migration to add image_url to movie table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130614004324) do
+ActiveRecord::Schema.define(version: 20130619194440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20130614004324) do
     t.integer  "rt_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_url"
   end
 
   create_table "user_opinions", force: true do |t|
