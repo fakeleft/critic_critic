1545d4510b1d5d1a70f207e2761bebc59759b36c
theoretick <mail@limitlesschannels.com>
2013-06-18 20:33:02 -0700

    temp revert, removed VCR

--- test/unit/api_seed_generator_test.rb
+++ test/unit/api_seed_generator_test.rb
@@ -9,51 +9,37 @@ describe 'ApiSeedGenerator' do
   it 'should get an api key' do
     @seed_generator.api_key.class.must_equal String
   end
-
   it 'should get the details for one movie by title' do
-    VCR.use_cassette('seed_search_movies') do
-      movie = @seed_generator.search_movies("Fried&Green&Tomatoes", 1)
-      movie.class.must_equal Hash
-      movie["movies"][0]["title"].must_equal "Fried Green Tomatoes"
-    end
-  end
-
-  it 'should get the details for one movie by id' do
-    VCR.use_cassette('seed_get_details_for_one_movie_by_id') do
-      movie = @seed_generator.get_movie(10104)
-      movie["title"].must_equal "Fried Green Tomatoes"
-    end
+    movie = @seed_generator.search_movies("Fried&Green&Tomatoes", 1)
+    movie.title.must_equal "Fried Green Tomatoes"
   end
 
   it 'should get the reviews for one movie by id' do
-    VCR.use_cassette('seed_get_reviews_for_one_movie_by_id') do
-      reviews = @seed_generator.get_reviews(10104)
-      reviews["reviews"].wont_be_nil
-    end
+    reviews = @seed_generator.get_movie_reviews(10104)
+    reviews.first.like.wont_be_nil
   end
-
-  it 'should get a list of upcoming movie ids' do
-    VCR.use_cassette('seed_get_upcoming_movie_ids') do
-      movie_ids = @seed_generator.get_movie_ids
-      movie_ids.class.must_equal Array
-      movie_ids.empty?.must_equal false
-      movie_ids.length.must_equal 10
-    end
+  it 'should return the movie ids from a collection of movies' do
+    movies = @seed_generator.get_upcoming_movies
+    movie_ids = @seed_generator.get_movie_ids(movies)
+    movie_ids.empty?.must_equal false
+    movie_ids.first.class.must_equal Fixnum
   end
 
   # these tests are driving the refactor
 
 
   it 'should return a single RtMovie' do
-    movie = @seed_generator.search_movies_new("Fried&Green&Tomatoes", 1)
+    movie = @seed_generator.search_movies("Fried&Green&Tomatoes", 1)
     movie.class.must_equal RtMovie
     movie.title.must_equal "Fried Green Tomatoes"
     movie.year.must_equal 1991
     movie.rt_id.must_equal 10104
+    movie.synopsis.must_equal ""
+    movie.to_seed_string.must_equal "Movie.create!(title: \"Fried Green Tomatoes\", year: 1991, description: \"\", rt_id: 10104)"
   end
 
   it 'should return an array of MovieReviews' do
-    reviews = @seed_generator.get_movie_reviews_new(10104)
+    reviews = @seed_generator.get_movie_reviews(10104)
     reviews.class.must_equal Array
     this_review = reviews[0]
     this_review.class.must_equal RtReview
@@ -64,34 +50,28 @@ describe 'ApiSeedGenerator' do
     this_review.critic.publication.must_equal "Entertainment Weekly"
   end
 
+  it 'should return the seed string for a movie' do
+    movie = @seed_generator.search_movies("Laurence&Anyways", 1)
+    movie.to_seed_string.must_equal 'Movie.create!(title: "Laurence Anyways", year: 2012, description: "Laurence Anyways clip", rt_id: 771303201)'
+  end
+
+  it 'should return the seed string for a review' do
+    review_string = %Q|c = Critic.create!(name: "Owen Gleiberman", publication: "Entertainment Weekly")
+m = Movie.find_by_rt_id(10104)
+CriticOpinion.create!(like: "true", url: "http://www.ew.com/ew/article/0,,309135,00.html", critic: c, movie: m)|
+
+    reviews = @seed_generator.get_movie_reviews(10104)
+    reviews.first.to_seed_string.must_equal review_string
+  end
+
+  it 'should return upcoming movies' do
+    movies = @seed_generator.get_upcoming_movies
+    movies.first.year.wont_be_nil
+  end
+
+  it 'should get the details for one movie by id' do
+    movie = @seed_generator.get_movie(10104)
+    movie.title.must_equal "Fried Green Tomatoes"
+  end
+
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
