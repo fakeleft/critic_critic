69cdc7b94380c86eaf2c68e28d8dd223f1691a3c
Dustin Zeisler <dustin@zive.me>
2013-06-20 16:08:28 -0700

    remove notice 'User was successfully updated.
    ' when submiting a rating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
     # end
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: '' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
