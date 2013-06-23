786ca4406e1ba7282212925adec48cd47e4d61a7
Dustin Zeisler <dustin@zive.me>
2013-06-22 19:53:42 -0700

    add feature, let user rate movie from show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,6 +14,7 @@ class MoviesController < ApplicationController
   # GET /movies/1.json
   def show
     @opinions = CriticOpinion.where({ :movie_id => @movie.id })
+    @user = User.first
   end
 
   def create
