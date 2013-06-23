483e26434d24432feb3e8615d24114e12af9ff7a
Dustin Zeisler <dustin@zive.me>
2013-06-22 19:24:16 -0700

    made movie search case insensitive

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.search(params[:search])
+    @movies = Movie.find(:all, :conditions => ["title ~* '.*#{params['search']}*.'"])
   end
   # GET /movies/1
   # GET /movies/1.json
